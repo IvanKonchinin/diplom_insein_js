15a070398270975b52df7c664647eea59e8edcee
Ivan Konchinin <vanish7@mail.ru>
2020-09-23 23:45:47 +0300

    feat:make smoothScroll

--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -1,9 +1,22 @@
 const toggleMenu = () => {
-  const popupMenu = document.querySelector('.popup-menu');
-  const popupDialogMenu = document.querySelector('.popup-dialog-menu');
+  const popupMenu = document.querySelector('.popup-menu'),
+        popupDialogMenu = document.querySelector('.popup-dialog-menu');
 
   const handlerMenu = () => {
     popupDialogMenu.classList.toggle('showHide-menu');
+  }
+
+  const scrollMenuItems = (e) => {
+
+    let target = e.target;
+    target = (target.tagName !== 'A') ? target.closest('a') : target;
+    e.preventDefault();
+    let itemHrefAttr = target.getAttribute('href');
+    let blockSearch = document.querySelector(`${itemHrefAttr}`);
+    if (blockSearch) blockSearch.scrollIntoView({
+      block: 'center',
+      behavior: 'smooth'
+    });
   }
 
   document.addEventListener('click', (e) => {
@@ -16,6 +29,14 @@ const toggleMenu = () => {
     if (target.matches('.close-menu')) {
       popupMenu.style.visibility = 'hidden';
       handlerMenu();
+    }
+    if ((target.tagName === 'A' && target.closest('popupDialogMenu')) || target.closest('a') && target.closest('.popup-menu')) {
+      scrollMenuItems(e);
+      popupMenu.style.visibility = 'hidden';
+      handlerMenu();
+    }
+    if (target.closest('.button-footer')){
+      scrollMenuItems(e);
     }
   });
 };
